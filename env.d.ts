f9a5fb5eb084d128614ac0cdfb1271588813e1e1
David Deak <david.deak1997@gmail.com>
2026-05-27 14:47:44 +0200

    Updated tsconfig files

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+import type { VNode } from 'vue'
+
+declare global {
+	namespace JSX {
+		interface Element extends VNode {}
+		interface ElementClass {
+			$props: Record<string, unknown>
+		}
+		interface IntrinsicElements {
+			[elem: string]: unknown
+		}
+	}
+}
